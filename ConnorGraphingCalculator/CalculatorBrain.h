06aa1fc7715a966d4fbf33a93620943750def15d
Connor Smith <cmds4410@gmail.com>
2011-10-13 18:03:55 -0400

    added files

---
+++ ConnorGraphingCalculator/CalculatorBrain.h
@@ -0,0 +1,42 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Connor Smith on 9/12/11.
+//  Copyright 2011 Bowdoin College. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject{
+    double operand;
+    double store;
+    NSString *waitingOperation;
+    NSString *variable;
+    double waitingOperand;
+    NSMutableArray *internalExpression;
+    NSMutableArray *myCopy;
+    NSNumber *operandSet;
+}
+@property (nonatomic) double operand;
+@property (nonatomic) double store;
+@property (copy) NSString *waitingOperation;
+@property (nonatomic) double waitingOperand;
+@property (readonly) id expression;
+@property (readonly, nonatomic)  NSDictionary *variableDictionary;
+
+
+
+- (void) setOperand:(double)aDouble;
+- (double) performOperation:(NSString *)operation;
+- (double) doMem:(NSString *)memOperation;
+- (void) setVariableAsOperand:(NSString *)variableName;
+
++ (double)evaluateExpression:(id)anExpression usingVariableValues:(NSDictionary *)variables;
+
++ (NSSet *)variablesInExpression:(id)anExpression;
++ (NSString *)descriptionOfExpression:(id)anExpression;
++ (id)propertyListForExpression:(id)anExpression; 
++ (id)experessionForPropertyList:(id)propertyList;
+
+@end
---
+++ ConnorGraphingCalculator/CalculatorBrain.m
@@ -0,0 +1,184 @@
+//
+//  CalculatorBrain.m
+//  Calculator
+//
+//  Created by Connor Smith on 9/12/11.
+//  Copyright 2011 Bowdoin College. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+#define VARIABLE_PREFIX @"var"
+
+@implementation CalculatorBrain
+@synthesize operand, store, waitingOperand, waitingOperation, expression, variableDictionary;
+- (id)init
+{
+    self = [super init];
+    internalExpression = [[NSMutableArray alloc] init];
+    
+    
+    return self;
+}
+- (void) dealloc
+{
+    [variableDictionary release];
+    [internalExpression release];
+    [super dealloc];
+}
+- (id) expression{
+    myCopy = [internalExpression copy];
+    [myCopy autorelease];
+    return myCopy;
+}
+//sets the operand, as well as appending the expression    
+- (void) setOperand:(double)aDouble{
+    operand = aDouble;
+    [internalExpression addObject:[NSNumber numberWithDouble:operand]];
+    NSLog(@"the expression is now: %@",internalExpression);
+}
+//adds a var to the expression
+- (void) setVariableAsOperand:(NSString *)variableName{
+    variable = [VARIABLE_PREFIX stringByAppendingString:variableName];
+    [internalExpression addObject:(NSString *)variable];
+    
+}
+//called by solve button, evaluates expressions with variable(s)
++ (double)evaluateExpression:(id)anExpression usingVariableValues:(NSDictionary *)variables
+{
+    NSLog(@"Expression passed to me: %@", anExpression);
+    CalculatorBrain *brain = [[CalculatorBrain alloc] init];
+    for (id element in anExpression) {
+        //adds digit to new operation
+        if ([element isKindOfClass:[NSNumber class]])
+            [brain setOperand:[element doubleValue]];
+        //if it's a variable, add it's true value
+        else if ([element isKindOfClass:[NSString class]])
+            if ([element rangeOfString:@"var"].location!=NSNotFound){
+                double varValue = [[variables objectForKey:element] doubleValue];
+                [brain setOperand:varValue];
+            }
+        //otherwise it's a string so it must be an operation
+            else [brain performOperation: element];
+        
+    }
+    [brain autorelease];
+    return brain.operand;
+    
+}
+//returns number of vars in expression, or nil of there are none
++ (NSSet *)variablesInExpression:(id)anExpression{
+    NSMutableSet *tempSet = [[NSMutableSet alloc] init];
+    for (id element in anExpression) {
+        if ([element isKindOfClass:[NSString class]])
+            if ([element rangeOfString:@"var"].location!=NSNotFound){
+                [tempSet addObject:element];
+            }
+    }
+    if ([tempSet count] == 0) {
+        return nil;
+    }
+    [tempSet autorelease];
+    return tempSet;
+}
+//returns string version of expression array
++ (NSString *)descriptionOfExpression:(id)anExpression{
+    //String that we return, even though it's mutable...?
+    NSMutableString *tempString = [[NSMutableString alloc] init];
+    for (id element in anExpression) {
+        //add string version of double to string
+        if ([element isKindOfClass:[NSNumber class]]){
+           [tempString appendString:[NSString stringWithFormat:@"%g", [element doubleValue]]];
+        }
+        //add string, but strip off the prefix constant if it is a variable
+       else if([element isKindOfClass:[NSString class]]){
+           if ([element hasPrefix:@"var"])
+               element = [element substringFromIndex:3];
+           [tempString appendString:element];
+       }
+    }           
+    NSLog(@"temp string is: %@",tempString);
+    return [tempString autorelease];
+}
+//for all memory functionality
+- (double) doMem:(NSString *)memOperation{
+    //store the display operand in a variable called 'store'
+    if ([memOperation isEqual:@"sto"]) {
+        store = operand;
+        //NSLog(@"%f", store);
+    }
+    if ([memOperation isEqual:@"mem+"]) {
+        store += operand;
+    }
+    if ([memOperation isEqual:@"CLR"]) {
+        store = 0;
+        [internalExpression removeAllObjects];
+    }
+    if ([memOperation isEqual:@"CLR MEM"]) {
+        store = 0;
+    }
+    //Rec's only functionality is to return 'store' so we don't need an if-claus for it
+    return store;
+    
+}
+- (void)performWaitingOperation
+{
+    if ([@"+" isEqual:waitingOperation]) {
+        operand = waitingOperand + operand; }
+    else if ([@"*" isEqual:waitingOperation]) {
+        operand = waitingOperand * operand; }
+    else if ([@"-" isEqual:waitingOperation]) {
+        operand = waitingOperand - operand; }
+    else if ([@"/" isEqual:waitingOperation]) {
+        if (operand) {
+            operand = waitingOperand / operand;
+        } 
+    }
+}
+
+
+- (double) performOperation:(NSString *)operation{
+    [internalExpression addObject:(NSString *) operation];
+    if ([operation isEqual:@"sqrt"]) {
+        operand = sqrt(operand);
+    }
+    //I added all of the following operations:
+    else if ([operation isEqual:@"1/x"] && operand != 0)
+    {
+        operand = 1/(operand);
+    }
+    else if ([operation isEqual:@"+/-"])
+    {
+        operand = -1*(operand);
+    }
+    else if ([operation isEqual:@"sin"])
+    {
+        operand = sin(operand);
+    }
+    else if ([operation isEqual:@"cos"])
+    {
+        operand = cos(operand);
+    }
+    else if ([operation isEqual:@"CLR"])
+    {
+        operand = 0;
+        waitingOperand = 0;
+        waitingOperation = nil;
+        
+        
+    }
+    else
+    {
+        [self performWaitingOperation];
+        waitingOperation = operation;
+        waitingOperand = operand;
+    }
+    return operand;
+}
+//not quite sure why we have these, maybe they're for later
++ (id)propertyListForExpression:(id)anExpression{
+    return anExpression;
+}
++ (id)experessionForPropertyList:(id)propertyList{
+    return propertyList;
+}
+@end
