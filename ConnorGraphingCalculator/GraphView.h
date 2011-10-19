58cb53e551d704f598c9dc6ba992af717f562261
Connor Smith <cmds4410@gmail.com>
2011-10-18 21:02:00 -0400

    Implemented Axes

---
+++ ConnorGraphingCalculator/GraphView.h
@@ -0,0 +1,13 @@
+//
+//  Graph View.h
+//  ConnorGraphingCalculator
+//
+//  Created by Connor Smith on 10/18/11.
+//  Copyright 2011 Bowdoin College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Graph_View : UIView
+
+@end
---
+++ ConnorGraphingCalculator/GraphView.m
@@ -0,0 +1,31 @@
+//
+//  Graph View.m
+//  ConnorGraphingCalculator
+//
+//  Created by Connor Smith on 10/18/11.
+//  Copyright 2011 Bowdoin College. All rights reserved.
+//
+
+#import "Graph View.h"
+
+@implementation Graph_View
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
